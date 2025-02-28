866c84abd88e422bd3162fa1801dc4fdece8b172
DelaBandara <kanishkadela@gmail.com>
2025-02-28 21:21:45 +0530

    Revert "header"
    This reverts commit eff65afea0f823c9c03141e297e686f8e70feba1.

--- online-grocery-store/app/layout.js
+++ online-grocery-store/app/layout.js
@@ -1,6 +1,5 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import Header from "./_components/Header";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
@@ -12,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={outfit.className}>
-        <Header />
-        {children
-        }</body>
+      <body className={outfit.className}>{children}</body>
     </html>
   );
 }
--- online-grocery-store/app/page.js
+++ online-grocery-store/app/page.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h2>home</h2>
-    
+    <h2>Dela</h2>
+    <Button>subscribe</Button>
    </div>
   );
 }
