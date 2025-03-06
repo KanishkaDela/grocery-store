d04f073f874f63f93d2bc2fbadc89e3f6a96a5fa
DelaBandara <kanishkadela@gmail.com>
2025-03-06 18:40:56 +0530

    category dropdown (Strapi get API)

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,8 +1,9 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { LayoutGrid, Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../_utils/GlobalApi'
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -14,6 +15,21 @@ import {
 
 
 function Header() {
+
+    const [categoryList,setCategoryList] = useState([]);
+    useEffect(()=>{
+        getCategoryList();
+    },[])
+
+    /**get category list */
+
+    const getCategoryList=()=>{
+        GlobalApi.getCategory().then(resp=>{
+            setCategoryList(resp.data.data);
+            console.log(resp.data.data)
+        })    
+    }
+
   return (
     <div className='p-5 shadow-sm flex justify-between'>
         <div className='flex items-center gap-8'>
@@ -30,10 +46,19 @@ function Header() {
                     <DropdownMenuContent>
                         <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        {categoryList.map((category,index) => (
+                            <DropdownMenuItem className="flex gap-4items-center cursor-pointer">
+                                <Image src={
+                                    process.env.NEXT_PUBLIC_BACKEND_BASE_URL+category?.icon?.[0]?.url} 
+                                    width={30} 
+                                    height={30}
+                                    alt='icon'
+                                    unoptimized={true}
+                                />
+                                <h2 className='text-lg'>{category?.name}</h2>
+                            </DropdownMenuItem>
+                        ))}
+                        
                     </DropdownMenuContent>
             </DropdownMenu>
 
