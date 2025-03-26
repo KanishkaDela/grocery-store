82e21af771be97b8ad77689f99d05424fa1fe0df
DelaBandara <kanishkadela@gmail.com>
2025-03-26 14:57:16 +0530

    checkout page

--- app/(routes)/checkout/page.jsx
+++ app/(routes)/checkout/page.jsx
@@ -1,40 +1,111 @@
+"use client"
+import GlobalApi from '@/app/_utils/GlobalApi';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { ArrowBigRight } from 'lucide-react';
-import React from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 function Checkout() {
+
+    const [user, setUser] = useState(null);
+    const [jwt, setJwt] = useState(null);
+    const [isLogin, setIsLogin] = useState(false);
+    const [totalCartItem,setTotalCartItem]=useState(0)
+    const [cartItemList,setCartItemList]=useState([]);
+    const [subtotal,setSubTotal]=useState(0);
+    const router=useRouter();
+
+    const [username,setUsername]=useState();
+    const [email,setEmail]=useState();
+    const [phone,setPhone]=useState();
+    const [zip,setZip]=useState();
+    const [address,setAddress]=useState();
+     
+          /**used to get total cart item */
+          const getCartItems=async()=>{
+              const cartItemList_=await GlobalApi.getCartItems(user.id,jwt)
+              console.log(cartItemList_);
+              setTotalCartItem(cartItemList_?.length);
+              setCartItemList(cartItemList_);
+          }
+
+          useEffect(() => {
+            if (typeof window !== 'undefined') {
+                const storedJwt = sessionStorage.getItem('jwt');
+                const storedUser = sessionStorage.getItem('user');
+        
+                if (!storedJwt || !storedUser) {
+                    router.push('/sign-in'); // Redirect if not logged in
+                } else {
+                    setIsLogin(true);
+                    setJwt(storedJwt);
+                    setUser(JSON.parse(storedUser));
+                }
+            }
+        }, []);
+        
+
+        useEffect(() => {
+            if (user && jwt) {
+                getCartItems();
+            }
+        }, [user, jwt]);
+
+        
+        
+            useEffect(()=>{
+                let total=0;
+                cartItemList.forEach(element => {
+                    total=total+element.amount
+                });
+                setSubTotal(total.toFixed(2))
+            },[cartItemList])
+        
+            const calculateTotalAmount = () => {
+                const validSubtotal = Number(subtotal) || 0; // Convert subtotal to a number, default to 0
+                const totalAmount = validSubtotal + (validSubtotal * 0.09 + 1500);
+                return totalAmount.toFixed(2);
+            };
+
+            const calculateTaxAmount=()=>{
+                const taxAmount=subtotal*0.09
+                return taxAmount.toFixed(2);
+            }
+            
+            
+
   return (
     <div>
       <h2 className='p-3 bg-primary text-xl font-bold text-center text-white'>Checkout</h2>
       <div className='p-5 px-5 md:px-10 grid grid-cols-1 md:grid-cols-3 py-8'>
-        <div className='col-span-2 mx-20'>
+        <div className='md:col-span-2 mx-20'>
             <h2 className='font-bold text-3xl'>Billing Details</h2>
             <div className='grid grid-cols-2 gap-10 mt-3'>
-                <Input placeholder='Name' />
-                <Input placeholder='Email' />
+                <Input placeholder='Name' onChange={(e)=>setUsername(e.target.value)} />
+                <Input placeholder='Email' onChange={(e)=>setEmail(e.target.value)} />
             </div>
             <div className='grid grid-cols-2 gap-10 mt-3'>
-                <Input placeholder='Phone' />
-                <Input placeholder='Zip' />
+                <Input placeholder='Phone' onChange={(e)=>setPhone(e.target.value)} />
+                <Input placeholder='Zip' onChange={(e)=>setZip(e.target.value)} />
             </div>
             <div className='mt-3'>
-                <Input placeholder='Address' />
+                <Input placeholder='Address' onChange={(e)=>setAddress(e.target.value)} />
             </div>
         </div>
         <div className='mx-10 border'>
-            <h2 className='p-3 bg-gray-200 font-bold text-center'>Total Cart (03)</h2>
+            <h2 className='p-3 bg-gray-200 font-bold text-center'>Total Cart {totalCartItem}</h2>
             <div className='p-4 flex flex-col gap-2'>
-                <h2 className='font-bold flex justify-between'>Subtotal : <span>Rs.25000.00</span></h2>
+                <h2 className='font-bold flex justify-between'>Subtotal : <span>Rs.{subtotal}</span></h2>
                 <hr></hr>
                 <h2 className='flex justify-between'>
                     Delivery : <span>Rs.1500.00</span>
                 </h2>
                 <h2 className='flex justify-between'>
-                    Tax : <span>Rs.25000.00</span>
+                    Tax (9%) : <span>Rs.{calculateTaxAmount()}</span>
                 </h2>
                 <hr></hr>
-                <h2 className='font-bold flex justify-between'>Total : <span>Rs.35000.00</span></h2>
+                <h2 className='font-bold flex justify-between'>Total : <span>Rs.{calculateTotalAmount()}</span></h2>
                 <Button>Payment <ArrowBigRight /> </Button>
             </div>
         </div>
